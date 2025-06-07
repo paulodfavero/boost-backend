08d82d60fac4b1f71d94181d20ed2182d61ccf3a
Paulo Favero <paulofavero@penzack.com>
2025-06-07 18:25:29 -0300

    feat: statments

--- src/use-cases/factories/make-search-credit-use-case.ts
+++ src/use-cases/factories/make-search-credit-use-case.ts
@@ -1,7 +1,13 @@
 import { PrismaCreditRepository } from '@/repositories/prisma/credit-repository'
-import { SearchCreditUseCase, SearchCreditCardListUseCase } from '../search-credits'
+import {
+  SearchCreditUseCase,
+  SearchCreditCardListUseCase,
+} from '../search-credits'
 import { PrismaExpenseRepository } from '@/repositories/prisma/expense-repository'
-import { PrismaBanksRepository, PrismaBankTypeAccountRepository } from '@/repositories/prisma/bank-repository'
+import {
+  PrismaBanksRepository,
+  PrismaBankTypeAccountRepository,
+} from '@/repositories/prisma/bank-repository'
 
 export function makeSearchCreditUseCase() {
   const creditRepository = new PrismaCreditRepository()
@@ -9,13 +15,17 @@ export function makeSearchCreditUseCase() {
   const bankRepository = new PrismaBanksRepository()
   const bankTypeAccountRepository = new PrismaBankTypeAccountRepository()
 
-  const useCase = new SearchCreditUseCase(creditRepository, expenseRepository, bankRepository, bankTypeAccountRepository)
+  const useCase = new SearchCreditUseCase(
+    creditRepository,
+    expenseRepository,
+    bankRepository,
+    bankTypeAccountRepository,
+  )
 
   return useCase
 }
 export function makeSearchCreditCardListUseCase() {
   const bankTypeAccountRepository = new PrismaBankTypeAccountRepository()
-
 
   const useCase = new SearchCreditCardListUseCase(bankTypeAccountRepository)
 
--- src/use-cases/search-credits.ts
+++ src/use-cases/search-credits.ts
@@ -1,11 +1,15 @@
 import { CreditsRepository } from '@/repositories/credit-repository'
 import { ExpensesRepository } from '@/repositories/expense-repository'
 import { addMonths, format, subMonths } from 'date-fns'
-import { BanksRepository, BanksTypeAccountRepository } from '@/repositories/bank-repository'
+import {
+  BanksRepository,
+  BanksTypeAccountRepository,
+} from '@/repositories/bank-repository'
 
 interface SearchCreditsUseCaseRequest {
   organizationId: string
   date: string
+  bankId?: string
 }
 
 export class SearchCreditUseCase {
@@ -14,11 +18,12 @@ export class SearchCreditUseCase {
     private ExpensesRepository: ExpensesRepository,
     private BankRepository: BanksRepository,
     private BankTypeAccountRepository: BanksTypeAccountRepository,
-  ) { }
+  ) {}
 
   async execute({
     organizationId,
     date,
+    bankId,
   }: SearchCreditsUseCaseRequest): Promise<object> {
     let totalCredits = 0
     let receivedCredits = 0
@@ -43,6 +48,7 @@ export class SearchCreditUseCase {
     const creditsFormated = await this.CreditsRepository.searchMany(
       organizationId,
       date,
+      bankId,
     )
     const currentExpense = await this.ExpensesRepository.searchMany(
       organizationId,
@@ -59,10 +65,12 @@ export class SearchCreditUseCase {
     const previousCredit = await this.CreditsRepository.searchMany(
       organizationId,
       previousMonth,
+      bankId,
     )
     const nextCredit = await this.CreditsRepository.searchMany(
       organizationId,
       nextMonth,
+      bankId,
     )
 
     previousExpense.map(({ amount, paid }) => {
@@ -93,54 +101,56 @@ export class SearchCreditUseCase {
       return true
     })
 
-    const credits = await Promise.all(creditsFormated.map(
-      async ({
-        id,
-        expiration_date,
-        purchase_date,
-        balance_close_date,
-        description,
-        category,
-        company,
-        amount,
-        type_payment,
-        installment_current,
-        installment_total_payment,
-        group_installment_id,
-        paid,
-        bankId,
-        bankTypeAccountId
-      }) => {
-        totalCredits += amount
-        if (paid) receivedCredits += amount
-
-        const bankTypeAccount = bankTypeAccountId ?
-          await this.BankTypeAccountRepository.findById(bankTypeAccountId) :
-          null
-
-        const bank = bankId ?
-          await this.BankRepository.findById(bankId) :
-          null
-
-        return {
+    const credits = await Promise.all(
+      creditsFormated.map(
+        async ({
           id,
-          expirationDate: expiration_date,
-          purchaseDate: purchase_date,
-          balanceCloseDate: balance_close_date,
+          expiration_date,
+          purchase_date,
+          balance_close_date,
           description,
-          company,
           category,
+          company,
           amount,
-          typePayment: type_payment,
-          installmentCurrent: installment_current,
-          installmentTotalPayment: installment_total_payment,
-          groupInstallmentId: group_installment_id,
+          type_payment,
+          installment_current,
+          installment_total_payment,
+          group_installment_id,
           paid,
-          bank,
-          bankTypeAccount
-        }
-      },
-    ))
+          bankId,
+          bankTypeAccountId,
+        }) => {
+          totalCredits += amount
+          if (paid) receivedCredits += amount
+
+          const bankTypeAccount = bankTypeAccountId
+            ? await this.BankTypeAccountRepository.findById(bankTypeAccountId)
+            : null
+
+          const bank = bankId
+            ? await this.BankRepository.findById(bankId)
+            : null
+
+          return {
+            id,
+            expirationDate: expiration_date,
+            purchaseDate: purchase_date,
+            balanceCloseDate: balance_close_date,
+            description,
+            company,
+            category,
+            amount,
+            typePayment: type_payment,
+            installmentCurrent: installment_current,
+            installmentTotalPayment: installment_total_payment,
+            groupInstallmentId: group_installment_id,
+            paid,
+            bank,
+            bankTypeAccount,
+          }
+        },
+      ),
+    )
 
     return {
       result: [...credits],
@@ -169,18 +179,18 @@ export class SearchCreditUseCase {
   }
 }
 export class SearchCreditCardListUseCase {
-  constructor(
-    private BankTypeAccountRepository: BanksTypeAccountRepository,
-  ) { }
+  constructor(private BankTypeAccountRepository: BanksTypeAccountRepository) {}
 
   async execute({
     organizationId,
   }: SearchCreditsUseCaseRequest): Promise<object> {
-
-    const creditsFormated= await this.BankTypeAccountRepository.findByOrganizationId(
-      organizationId,
-    ) as any[]
-    const creditCardList =  creditsFormated.filter((card: any) => card.type === 'CREDIT')
-    return creditCardList 
+    const creditsFormated =
+      (await this.BankTypeAccountRepository.findByOrganizationId(
+        organizationId,
+      )) as any[]
+    const creditCardList = creditsFormated.filter(
+      (card: any) => card.type === 'CREDIT',
+    )
+    return creditCardList
   }
 }
