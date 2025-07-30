11d159c643b53342e25a7dbe66d2183d86e912b2
Paulo Favero <paulofavero@penzack.com>
2025-07-30 12:37:00 -0300

    feat: result credit card

--- src/use-cases/factories/make-search-results-use-case.ts
+++ src/use-cases/factories/make-search-results-use-case.ts
@@ -1,12 +1,18 @@
 import { PrismaExpenseRepository } from '@/repositories/prisma/expense-repository'
 import { PrismaGainRepository } from '@/repositories/prisma/gain-repository'
+import { PrismaCreditRepository } from '@/repositories/prisma/credit-repository'
 import { SearchResultsUseCase } from '../search-results'
 
 export function makeSearchResultUseCase() {
   const expenseRepository = new PrismaExpenseRepository()
   const gainRepository = new PrismaGainRepository()
+  const creditRepository = new PrismaCreditRepository()
 
-  const useCase = new SearchResultsUseCase(expenseRepository, gainRepository)
+  const useCase = new SearchResultsUseCase(
+    expenseRepository,
+    gainRepository,
+    creditRepository,
+  )
 
   return useCase
 }
