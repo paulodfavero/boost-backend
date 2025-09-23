78a58438ebcb136f317318f8258074e12bd6aa38
Paulo Favero <paulofavero@penzack.com>
2025-09-23 09:53:21 -0300

    feat: all categories

--- src/use-cases/search-subcategories.ts
+++ src/use-cases/search-subcategories.ts
@@ -1,11 +1,26 @@
 import { SubCategoriesRepository } from '@/repositories/subcategory-repository'
+import { CategoriesCreditCardRepository } from '@/repositories/category-credit-card-repository'
 
 export class SearchSubcategoryUseCase {
-  constructor(private subcategoriesRepository: SubCategoriesRepository) {}
+  constructor(
+    private subcategoriesRepository: SubCategoriesRepository,
+    private categoriesCreditCardRepository: CategoriesCreditCardRepository,
+  ) {}
 
   async execute(): Promise<object> {
-    const categories = await this.subcategoriesRepository.searchMany()
+    const [subcategories, categoriesCreditCard] = await Promise.all([
+      this.subcategoriesRepository.searchMany(),
+      this.categoriesCreditCardRepository.searchMany(),
+    ])
 
-    return categories
+    // Concatenate categoriesCreditCard with their subcategories
+    const result = categoriesCreditCard.map((category) => ({
+      ...category,
+      subcategories: subcategories.filter(
+        (sub) => sub.categoryCreditCardId === category.id,
+      ),
+    }))
+
+    return result
   }
 }
